273685702b14c37f1349118525dbd80892968b3e
Peter Knauber <p.knauber@me.com>
2015-03-12 11:53:01 +0100

    Code und Testcode hinzugefügt

---
+++ src/uebung04/Conversion.java
@@ -0,0 +1,28 @@
+package uebung04;
+
+import static gdi.MakeItSimple.*;
+
+public class Conversion {
+	
+	static int parseInteger(String digits) {
+		int number = 0;
+		int i = 0;
+		while (i < digits.length()) {
+			number = number * 10 + digits.charAt(i) - '0';
+			i++;
+		}
+		if (digits.equals(number + ""))
+			return number;
+		else
+			throw new GDIException("Keine gültige Eingabe");
+	}
+	
+	
+	public static void main(String[] args) {
+		print("Geben Sie eine Zahl ein (wird als String gelesen): ");
+		String numberAsString = readLine();
+		
+		println("Gewandelte Zahl: " + parseInteger(numberAsString));
+	}
+
+}
---
+++ src/uebung04/ConversionTest.java
@@ -0,0 +1,84 @@
+package uebung04;
+
+import static org.junit.Assert.*;
+import gdi.MakeItSimple.GDIException;
+
+import org.junit.Test;
+
+public class ConversionTest {
+
+	@Test
+	public void gültige() throws Exception {
+		assertEquals(0, Conversion.parseInteger("0"));
+		assertEquals(1, Conversion.parseInteger("1"));
+		assertEquals(1234567890, Conversion.parseInteger("1234567890"));
+		assertEquals(Integer.MAX_VALUE - 1, Conversion.parseInteger(Integer.MAX_VALUE - 1 + ""));
+		assertEquals(Integer.MAX_VALUE, Conversion.parseInteger(Integer.MAX_VALUE + ""));
+	}
+
+	@Test(expected=GDIException.class)
+	public void zuGroßeZahlVerursachtÜberlauf() throws Exception {
+		System.err.println("Exception 'Zahl zu groß' erwartet; geliefert: " + Conversion.parseInteger((long) Integer.MAX_VALUE + 1 + ""));
+	}
+
+	@Test(expected=GDIException.class)
+	public void negativ() throws Exception {
+		Conversion.parseInteger("-1");
+	}
+
+	@Test(expected=GDIException.class)
+	public void minValueIstNegativ() throws Exception {
+		Conversion.parseInteger(Integer.MIN_VALUE + "");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen1() throws Exception {
+		Conversion.parseInteger("x");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen2() throws Exception {
+		Conversion.parseInteger("-12");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen3() throws Exception {
+		Conversion.parseInteger("1-2");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen4() throws Exception {
+		Conversion.parseInteger("12-");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen5() throws Exception {
+		Conversion.parseInteger("+12");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen6() throws Exception {
+		Conversion.parseInteger("1+2");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen7() throws Exception {
+		Conversion.parseInteger("12+");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen8() throws Exception {
+		Conversion.parseInteger(" 12");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen9() throws Exception {
+		Conversion.parseInteger("1 2");
+	}
+
+	@Test(expected=GDIException.class)
+	public void ungültigeZeichen10() throws Exception {
+		Conversion.parseInteger("12 ");
+	}
+
+}
